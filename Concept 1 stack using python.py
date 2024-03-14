29a5ca8269ff12f975c98b659dc53dd8fe7e9a09
prajun v <prajunvelandi@gmail.com>
2024-03-14 17:24:39 +0530

    created a simple stack using python list

--- Concept 1 stack using python.py
+++ Concept 1 stack using python.py
@@ -7,5 +7,28 @@ push and pop are the most are the important operations on stack
 append method is used for push operation
 pop method is used for pop operation
 
+boolean form of an empty list is False
+that is
+a = []
+print(bool(a)) -> False
+
 
 """
+class stack:
+    def __init__(self):
+        self.stack = []
+
+    def stackAppend(self, data):
+        self.stack.append(data)
+
+    def stackPop(self):
+        if not self.stack:
+            print('stack is empty')
+
+        else:
+            poppedElement = self.stack.pop()
+            return poppedElement
+    def printStack(self):
+        print(self.stack)
+
+
