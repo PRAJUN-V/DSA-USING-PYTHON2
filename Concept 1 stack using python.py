18bdf81da6f605a3714b53578e05bbb49d800429
prajun v <prajunvelandi@gmail.com>
2024-03-14 17:06:45 +0530

    added file named stack

---
+++ Concept 1 stack using python.py
@@ -0,0 +1,11 @@
+"""
+In python stack can be implemented using two methods
+1. using list
+2. using packages
+
+push and pop are the most are the important operations on stack
+append method is used for push operation
+pop method is used for pop operation
+
+
+"""
